fe37c2e9941f99efb42452c0fe327bf2bf1e45d6
harithdarwish <harithdarwish49@gmail.com>
2026-08-31 02:59:58 +0800

    add terraform2

--- terraform2/providers.tf
+++ terraform2/providers.tf
@@ -6,6 +6,16 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+
+  backend "s3" {
+    bucket       = "bootcamp-2026-harith" #use existed s3 bucket
+    key          = "terraform2/terraform.tfstate"
+    region       = "ap-southeast-1"
+    use_lockfile = true
+  }
+
+
 }
 
 provider "aws" {
